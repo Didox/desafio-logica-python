773922f7ac829990f954eea2d2077922f8bf490a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-07-26 06:22:12 -0300

    iniciando desafio de logica em python

---
+++ exercicio1.py
@@ -0,0 +1,26 @@
+# Dados que o professor Danilo, precise mostrar algumas informações 
+# importantes na tela, faça um programa que capture estas informações
+# e mostre na tela de forma organizada, exemplo de organização:
+
+# ========== Programa de formatação de dados ==============
+# Nome: XXXXXX
+# Telefone: XXXXXXX
+# Endereço: XXXXXXX
+# =========================================================
+import os
+
+print("================ Bem vindo ao sistema de cadastro =================")
+print("Digite o seu nome:")
+nome = input()
+print("Digite o seu telefone:")
+telefone = input()
+print("Digite o seu endereço:")
+endereco = input()
+
+os.system('clear')
+
+print("========== Programa de formatação de dados ==============")
+print("Nome: " + nome)
+print("Telefone: " + telefone)
+print("Endereço: " + endereco)
+print("=========================================================")
---
+++ variaveis.py
@@ -0,0 +1,10 @@
+print("Digite algo na tela") # Mostre
+algo = input() # le informações 
+print("O valor digitado foi: " + algo) # mostrando o valor da variável na tela
+print(type(algo)) # mostrando o valor da variável na tela
+
+# ===== Tipos de dados mais utilizados =====
+# str  "252351vbs1vnsdnsdsdQQQdshsds#@5221!!#$44"
+# int 13213231
+# float 2.5 7.9 3.0
+# bool true false
